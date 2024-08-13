1273c6f5b35e70c54c52e82b06da8c32307c0897
Induwara Deshan Jayathilaka <122204893+InduwaraDeshan@users.noreply.github.com>
2024-08-13 20:16:17 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
   
 
     $stripeDetails = array(
-        "secretKey"=>"sk_test_51LiNE9ArMd2Z7KzyKJVxMS5SfqMO9Sdfau01EW0OQRJjhoqt13Nbetm3p6jNwpIBwdHykztF6bR6KCCtIv6M35ye00zSYvvqsW" ,
+        "secretKey"=> "scretKey" ,
         "publishableKey"=>"pk_test_51LiNE9ArMd2Z7KzyJnPKOnVQUfNRNbtPezLDceY2lNoPfw36Zep4Bdb6hrqufQrij4Saa4FkrHdxTU23xMowlPJJ00RAJsNffw"
     );
 
